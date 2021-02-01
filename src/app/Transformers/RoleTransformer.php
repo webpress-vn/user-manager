47311d0cf861d283e2aaaeb23bff7519769175d2
sonhaichu <sonhaichu@gmail.com>
2021-02-01 08:51:35 +0700

    feat: includes in role-transformer

--- src/app/Transformers/RoleTransformer.php
+++ src/app/Transformers/RoleTransformer.php
@@ -3,10 +3,14 @@
 namespace VCComponent\Laravel\User\Transformers;
 
 use League\Fractal\TransformerAbstract;
+use VCComponent\Laravel\User\Transformers\PermissionTransformer;
 
 class RoleTransformer extends TransformerAbstract
 {
-    protected $availableIncludes = [];
+    protected $availableIncludes = [
+        'permissions',
+        'users',
+    ];
 
     public function __construct($includes = [])
     {
@@ -22,11 +26,22 @@ class RoleTransformer extends TransformerAbstract
             'status'      => $model->status,
             'description' => $model->description,
             'level'       => $model->level,
-            'status'      =>$model->status,
+            'status'      => $model->status,
             'timestamps'  => [
                 'created_at' => $model->created_at,
                 'updated_at' => $model->updated_at,
             ],
         ];
+    }
+
+    public function includePermissions($model)
+    {
+        return $this->collection($model->permissions, new PermissionTransformer());
+    }
+
+    public function includeUsers($model)
+    {
+        $user_transformer = config('user.transformers.user');
+        return $this->collection($model->users, new $user_transformer());
     }
 }
