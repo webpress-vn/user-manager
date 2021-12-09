ab3bde747a21c2bb7c87c7a26941cc31c84e94a9
dev_duybv <duybv@vicdoers.com>
2021-12-09 15:00:03 +0700

    fix: user repository in controller

--- src/app/Http/Controllers/Web/SocialiteLoginController.php
+++ src/app/Http/Controllers/Web/SocialiteLoginController.php
@@ -5,7 +5,7 @@ namespace VCComponent\Laravel\User\Http\Controllers\Web;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
-use VCComponent\Laravel\User\Entities\User;
+use VCComponent\Laravel\User\Repositories\UserRepository;
 
 class SocialiteLoginController extends Controller
 {
@@ -14,6 +14,12 @@ class SocialiteLoginController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    protected $repository;
+    public function __construct(UserRepository $repository)
+    {
+        $this->repository = $repository;
+
+    }
     public function redirectToProvider($driver)
     {
         return Socialite::driver($driver)->redirect();
@@ -31,18 +37,18 @@ class SocialiteLoginController extends Controller
         } catch (\Exception $e) {
             return redirect()->route('login');
         }
-        $existingUser = User::where('email', $user->getEmail())->first();
+        $existingUser = $this->repository->findByField('email', $user->getEmail())->first();
         if ($existingUser) {
             auth()->login($existingUser, true);
         } else {
-            $newUser = new User;
-            $newUser->account_type = $driver;
-            $newUser->social_id = $user->getId();
-            $newUser->last_name = $user->getName();
-            $newUser->email = $user->getEmail();
-            $newUser->avatar = $user->getAvatar();
-            $newUser->verify_token = str::random(32);
-            $newUser->save();
+            $newUser = $this->repository->create([
+                'account_type' => $driver,
+                'social_id' => $user->getId(),
+                'first_name' => $user->getName(),
+                'email' => $user->getEmail(),
+                'avatar' => $user->getAvatar(),
+                'verify_token' => str::random(32),
+            ]);
             auth()->login($newUser, true);
         }
         return redirect('/');
